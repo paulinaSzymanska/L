3ff04b095377783cde40cc850709181e57bfd076
paulinaSzymanska <96289809+paulinaSzymanska@users.noreply.github.com>
2023-10-26 15:48:17 +0100

    Task added

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -1,4 +1,9 @@
 package src;
+//Dana jest lista o nazwie names, składająca się z sześciu różnych imion. Napisz program, który sprawdza,
+// ile imion zawiera pięć i więcej znaków. Program dodatkowo ma zliczać ich liczbe.
+//Przedstaw 2 rozwiązania:
+//imperatywne
+//funkcyjne
 
 public class FirstTask {
     public static void main(String[] args) {
